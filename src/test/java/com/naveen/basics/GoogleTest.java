01093a68d692fd27b5e0980888a0ce77df6d4bed
Naveen <naveen94.gotur@gmail.com>
2024-03-22 13:07:35 +0530

    first google test

---
+++ src/test/java/com/naveen/basics/GoogleTest.java
@@ -0,0 +1,21 @@
+package com.naveen.basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class GoogleTest {
+
+	public static void main(String[] args) {
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		System.out.println("Title-"+driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		
+		driver.close();
+	}
+
+}
